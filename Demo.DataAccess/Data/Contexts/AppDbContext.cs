e1a4867a1baa7e57b32a3a76f36990e03262c228
s3d0o <s3d0o@DESKTOP-K883P0O>
2025-09-16 06:12:40 +0300

    Video 4 , DAL => Department Entity

---
+++ Demo.DataAccess/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo.DataAccess.Data.Contexts
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Demo.DataAccess/Models/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo.DataAccess.Models
+{
+    internal class BaseEntity
+    {
+        public int Id { get; set; }
+        public int CreatedBy { get; set; } // user id
+        public int ModifiedBy { get; set; } // user id
+        public DateTime? CreatedAt { get; set; } // the date time of creation
+        public DateTime? ModifiedAt { get; set; } // the date time of modification
+        public bool IsDeleted { get; set; }
+    }
+}
