1cdf8b24f83a59772dc6211457690acfecda6beb
s3d0o <s3d0o@DESKTOP-K883P0O>
2025-10-07 03:34:17 +0300

    Video06

---
+++ Demo.Presentaion/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Demo.DataAccess.Models.IdentityModule;
+using Demo.Presentaion.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentaion.Controllers
+{
+    public class AccountController(UserManager<ApplicationUser> _userManager) : Controller
+    {
+        #region Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if(!ModelState.IsValid) return View(registerViewModel);
+            var user = new ApplicationUser()
+            {
+                UserName = registerViewModel.UserName,
+                FirstName = registerViewModel.FirstName,
+                LastName = registerViewModel.LastName,
+                Email = registerViewModel.Email,
+            };
+           var result = _userManager.CreateAsync( user ,registerViewModel.Password).Result;
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Login");
+            }
+            foreach(var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(registerViewModel);
+        }
+        #endregion
+
+        #region Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        #endregion
+        public IActionResult TermService()
+        {
+            return View();
+        }
+
+    }
+}
--- Demo.Presentaion/Program.cs
+++ Demo.Presentaion/Program.cs
@@ -6,6 +6,8 @@ using Demo.DataAccess.Data.Contexts;
 using Demo.DataAccess.Data.Repositories.Classes;
 using Demo.DataAccess.Data.Repositories.Interfaces;
 using Demo.DataAccess.Data.Repositories.Repositories;
+using Demo.DataAccess.Models.IdentityModule;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -42,6 +44,8 @@ namespace Demo.Presentaion
             builder.Services.AddScoped<IEmployeeService, EmployeeService>();
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddScoped<IAttachmentService, AttachemntService>();
+            builder.Services.AddIdentity<ApplicationUser,IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
             #endregion
 
             var app = builder.Build();
@@ -61,7 +65,7 @@ namespace Demo.Presentaion
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=Register}/{id?}");
 
             app.Run();
         }
